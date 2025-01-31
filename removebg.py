17736a0fa43227ba21c81aed3f19c182485723c0
Kristofer Borges <kristofer.souza2027@hotmail.com>
2025-01-31 14:53:28 -0300

    Novo exercícios, remove fundo das imagens

--- removebg.py
+++ removebg.py
@@ -6,14 +6,14 @@ import onnxruntime
 input_path = 'img/photo.jpg'
 output_path = 'img/result/imagem_sem_fundo.png'
 
-# Carregar a imagem de entrada
+# entrada
 with open(input_path, 'rb') as input_file:
     input_image = input_file.read()
 
-# Remover o fundo da imagem
+# remove o fundo da imagem
 output_image = remove(input_image)
 
-# Salvar a imagem resultante
+# salvar a imagem
 with open(output_path, 'wb') as output_file:
     output_file.write(output_image)
 
